28814d1be7760300e5e7915a9929f1c350e9794c
caddyz <18784227116@163.com>
2019-07-04 22:45:16 +0800

    增加iviewui

--- src/packageA/logs.vue
+++ src/packageA/logs.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <div v-for="i in 7" :key="i">
+      <i-card thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">内容不错</view>
+        <view slot="footer">尾部内容</view>
+      </i-card>
+    </div>
     <ul class="container log-list">
       <li v-for="(log, index) in logs" :class="{ red: aa }" :key="index" class="log-item">
         <card :text="(index + 1) + ' . ' + log"></card>
@@ -14,7 +20,11 @@ import card from '@/components/card'
 
 export default {
   config: {
-    navigationBarTitleText: '查看启动日志'
+    navigationBarTitleText: '查看启动日志',
+    usingComponents: {
+      'i-card': '../../static/iview/card/index',
+      'i-button': '../../static/iview/button/index'
+    }
   },
 
   components: {
@@ -34,7 +44,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .log-list {
   display: flex;
   flex-direction: column;
@@ -43,5 +53,8 @@ export default {
 
 .log-item {
   margin: 10rpx;
+}
+i-card {
+  width: 255rpx;
 }
 </style>
